7f8f56b51a49d95cd837d41d80251b070b733cb6
Elio Spaccarelli <espaccarelli@zagmail.gonzaga.edu>
2026-09-22 09:59:22 -0700

    CalculateWorkPower

---
+++ VO+IL.py
@@ -0,0 +1,8 @@
+force=float(input("Enter the force in Newtons: "))
+distance=float(input("Enter the distance in meters: "))
+time=float(input("Enter the time in seconds: "))
+work=force*distance
+print("Work:",work)
+power=work/time
+print("Power:",power)
+
---
+++ Variale_la.py
@@ -0,0 +1,7 @@
+force=100
+distance=5
+work=force*distance
+print("work",work)
+time=6
+power=work/time
+print("power",power)
